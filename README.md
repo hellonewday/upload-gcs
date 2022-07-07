898f937175dcf6860979585b15618c7e899b79d5
hungnq98 <hungnq10@vnpt.vn>
2022-07-07 15:56:20 +0700

    add instructions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # upload-gcs
 REST API for upload files on Google Cloud Storage
+
+# Note
+- This is an example code that saves file to MongoDB. However, depends on your tech stack, you can save to any database
+
+- Please Go to Google Cloud console and create a keystore file in order to run the example code. 
+
--- controllers/files.js
+++ controllers/files.js
@@ -1,8 +1,9 @@
 const File = require("../model/File");
 const { Storage } = require("@google-cloud/storage");
 
+// Generate key file like this in:  IAM & Admin > Service Accounts
 const storage = new Storage({
-  projectId: "flowing-coil-269914",
+  projectId: "flowing-coil-269914", // Replace with your project-id
   keyFilename: "./flowing-coil-269914-569572b276ae.json",
 });
 
@@ -17,7 +18,7 @@ module.exports.showFiles = (req, res) => {
 };
 
 module.exports.uploadFiles = (req, res, next) => {
-  const bucket = storage.bucket("my-warm-bucket");
+  const bucket = storage.bucket("my-warm-bucket"); // Replace with your bucket name
   const blob = bucket.file(req.file.originalname);
 
   const blobStream = blob.createWriteStream({
@@ -31,6 +32,7 @@ module.exports.uploadFiles = (req, res, next) => {
   blobStream.on("finish", () => {
     const fileUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
 
+    // Below code is optional 
     const input = new File({
       fileUrl,
       description: req.body.description,
@@ -43,6 +45,8 @@ module.exports.uploadFiles = (req, res, next) => {
         res.status(201).json({ success: true, response });
       }
     });
+
+    // End optional code. :)
   });
 
   blobStream.end(req.file.buffer);
